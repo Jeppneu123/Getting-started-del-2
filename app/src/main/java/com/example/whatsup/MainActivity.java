b92432a1ff2e07a602a6c0c20683ec56046cf2b1
Jeppeneu123 <jeppeneumann@live.dk>
2023-02-27 11:09:06 +0100

    Second Commit Android App Test

--- app/src/main/java/com/example/whatsup/MainActivity.java
+++ app/src/main/java/com/example/whatsup/MainActivity.java
@@ -3,12 +3,27 @@ package com.example.whatsup;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+    String TAG = this.getClass().getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d(TAG, "onCreate: App up and running");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause: Pausing the app");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume: Now resuming the application");
     }
 }
